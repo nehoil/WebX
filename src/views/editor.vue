3817677b92f4efe56e329f06096bef9aa90b45bc
roee-hoobian <hoobizone@gmail.com>
2020-11-25 20:39:55 +0200

    connection made between addCmp and web store

--- src/views/editor.vue
+++ src/views/editor.vue
@@ -9,6 +9,9 @@
 import json from "@/data/wap.json";
 import workSpace from "@/cmps/workspace.cmp";
 import controller from "@/cmps/controller.cmp";
+// import webStore from "@/store/web.store.js";
+import { eventBus } from "@/services/eventbus.service.js";
+
 export default {
   name: "editor",
   components: {
@@ -20,6 +23,12 @@ export default {
       waps: json,
       itemToEdit: "webImg",
     };
+  },
+  created() {
+    eventBus.$on("addCmp", (type) => {
+      console.log("add", type);
+      this.$store.commit({ type: "addCmp", cmpType: type })
+    });
   },
 };
 </script>
