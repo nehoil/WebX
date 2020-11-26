0023f6dac447f32a9631a177fa6054724a1d08d0
Nehoray Ilani <neho12@gmail.com>
2020-11-26 18:40:46 +0200

    added map service

--- src/cmps/controller.opts.cmps/map.cmp.vue
+++ src/cmps/controller.opts.cmps/map.cmp.vue
@@ -1,17 +1,40 @@
 <template>
   <div class="edit-map">
     <button>search location</button>
+        <el-input
+      v-model="input"
+      @input="searchLocation"
+      placeholder="Your address here"
+    ></el-input>
+
     <button>zoom</button>
     <button>name</button>
   </div>
 </template>
 
 <script>
+import { mapService } from '@/services/map.service.js'
+
+
 export default {
   name: "edit-map",
   components: {},
   data() {
-    return {};
+    return {
+      input: ''
+    };
+  },
+  methods: {
+    async searchLocation(){
+      const res = await mapService.getAddress('tel aviv')
+      try {
+        console.log('res from geocode api', res);
+      } catch {
+        console.log('error');
+      }
+    },
+    
+
   },
 };
 </script>
