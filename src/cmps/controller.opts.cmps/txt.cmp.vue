fa8fca3ca7975778e3749f88d66e502b48111ec9
haleli amiad <haleliamiad@h-MBP-sl-haleli.homenet.telecomitalia.it>
2020-11-28 10:22:43 +0100

    removed line height option

--- src/cmps/controller.opts.cmps/txt.cmp.vue
+++ src/cmps/controller.opts.cmps/txt.cmp.vue
@@ -3,7 +3,7 @@
     <select-color :cmp="cmp"></select-color>
     <select-font :cmp="cmp"></select-font>
     <font-properties :cmp="cmp"></font-properties>
-    <div class="flex center space plr10">
+    <!-- <div class="flex center space plr10">
       <span>Line Height</span>
       <el-slider
         :min="1"
@@ -11,7 +11,7 @@
         v-model="lineHeight"
         @input="setLineHeight"
       ></el-slider>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -19,7 +19,7 @@
 import selectFont from "./select-font.cmp";
 import selectColor from "./select-color.cmp";
 import fontProperties from "./font-properties.cmp";
-import { eventBus } from "../../services/eventbus.service.js";
+// import { eventBus } from "../../services/eventbus.service.js";
 export default {
   name: "edit-txt",
   props: {
@@ -32,35 +32,35 @@ export default {
   },
   data() {
     return {
-      fontSize: null,
-      lineHeight: null,
-      letterSpacing: null,
+      // fontSize: null,
+      // lineHeight: null,
+      // letterSpacing: null,
       textShadow: null,
       value: "",
     };
   },
   created() {
-    if (!this.cmp.style.lineHeight) {
-      this.cmp.style.lineHeight = "unset";
-    } else {
-      var lineHeightNum = this.getNumFromString("lineHeight");
-      this.lineHeight = +lineHeightNum * 16;
-    }
+    // if (!this.cmp.style.lineHeight) {
+    //   this.cmp.style.lineHeight = "normal";
+    // } else {
+    //   var lineHeightNum = this.getNumFromString("lineHeight");
+    //   this.lineHeight = +lineHeightNum * 16;
+    // }
   },
   methods: {
-    getNumFromString(styleProperty) {
-      if (this.cmp.style[styleProperty].match(/\d+/g).flat().length === 2) {
-        return this.cmp.style[styleProperty].match(/\d+/g).flat().join(".");
-      } else {
-        return this.cmp.style[styleProperty].match(/\d+/g).flat().join();
-      }
-    },
-    setLineHeight(size) {
-      this.lineHeight = size;
-      this.cmp.style.lineHeight = size / 16 + "rem";
-      eventBus.$emit("update-site");
-      this.lineHeight = null;
-    },
+    // getNumFromString(styleProperty) {
+    //   if (this.cmp.style[styleProperty].match(/\d+/g).flat().length === 2) {
+    //     return this.cmp.style[styleProperty].match(/\d+/g).flat().join(".");
+    //   } else {
+    //     return this.cmp.style[styleProperty].match(/\d+/g).flat().join();
+    //   }
+    // },
+    // setLineHeight(size) {
+    //   this.lineHeight = size;
+    //   this.cmp.style.lineHeight = size / 16 + "rem";
+    //   eventBus.$emit("update-site");
+    //   this.lineHeight = null;
+    // },
   },
 };
 </script>
