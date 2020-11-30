3f009bcf2031eae0ec811bc8378e5983e54a0c1e
roee-hoobian <hoobizone@gmail.com>
2020-11-30 18:19:47 +0200

    login tab added

--- src/app.vue
+++ src/app.vue
@@ -1,22 +1,42 @@
 <script>
-
+import loginCmp from "@/cmps/login.cmp";
+export default {
+  data() {
+    return {
+      isShowLogin: false,
+    };
+  },
+  components: {
+    loginCmp,
+  },
+  methods: {
+    showLogin() {
+      this.isShowLogin = !this.isShowLogin;
+    },
+  },
+};
 </script>
 
 <template>
   <div id="app" class="container">
     <div class="main-header">
-    <nav>
-            <div class="logo">
-       <router-link to="/"> <img src="./assets/logo.png" alt="" srcset=""></router-link>
-      </div>
-      <div class="menu">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/templates">Templates</router-link> |
-      <router-link to="/">Profile</router-link> |
-      <router-link to="/editor">Editor</router-link>
-      </div>
-    </nav>
+      <nav>
+        <div class="logo">
+          <router-link to="/">
+            <img src="./assets/logo.png" alt="" srcset=""
+          /></router-link>
+        </div>
+        <div class="menu">
+          <router-link to="/">Home</router-link> |
+          <router-link to="/templates">Templates</router-link> |
+          <router-link to="/">Profile</router-link> |
+          <router-link to="/editor">Editor</router-link> |
+          <span @click="showLogin" class="login-btn">Log In</span>
+        </div>
+      </nav>
     </div>
+    <div class="modal-bg" v-if="isShowLogin"></div>
+    <loginCmp v-if="isShowLogin" @showLogin="showLogin"/>
     <router-view />
   </div>
 </template>
@@ -29,5 +49,4 @@
   text-align: center;
   color: #2c3e50;
 }
-
 </style>
---
+++ src/cmps/login.cmp.vue
@@ -0,0 +1,67 @@
+<template>
+<section>
+<el-form ref="login-form" :model="login" label-width="120px" v-if="!isSignUp">
+  <el-form-item label="Name">
+    <el-input v-model="login.name"></el-input>
+  </el-form-item>
+  <el-form-item label="Password">
+    <el-input v-model="login.Password" show-password></el-input>
+  </el-form-item>
+  <el-form-item>
+    <el-button type="primary" @click="submitLogin">Login</el-button>
+    <el-button>Cancel</el-button>
+  <p>Not a member yet? <a @click="toggleSignUp">Sign up!</a></p>
+  </el-form-item>
+</el-form>
+
+  <el-form ref="form" :model="signup" label-width="120px" v-if="isSignUp">
+  <el-form-item label="Name">
+    <el-input v-model="signup.name"></el-input>
+  </el-form-item>
+  <el-form-item label="Mail">
+    <el-input v-model="signup.mail"></el-input>
+  </el-form-item>
+  <el-form-item label="Password">
+    <el-input v-model="signup.Password" show-password></el-input>
+  </el-form-item>
+  <el-form-item>
+    <el-button type="primary" @click="submitSignup">Sign up</el-button>
+    <el-button>Cancel</el-button>
+  <p>Not a member yet? <a @click="toggleSignUp">Sign up!</a></p>
+  </el-form-item>
+</el-form>
+</section>
+
+
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        isSignUp:false,
+        login: {
+          name: '',
+          Password:'',         
+        },
+        signup: {
+          name: '',
+          Password:'', 
+          mail:''        
+        }
+      }
+    },
+    methods: {
+      submitLogin() {
+        console.log(this.form.name, this.form.Password);
+        this.$emit('showLogin')
+      },
+    toggleSignUp(){
+      this.isSignUp = !this.isSignUp
+    },
+    submitSignup(){
+      console.log('signup');
+      
+    }
+    },
+  }
+</script>
