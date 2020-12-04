39423a3bf6e0a12470e03eab9a6cfcc0f15c696b
haleli amiad <haleliamiad@h-MBP-sl-haleli.homenet.telecomitalia.it>
2020-12-04 13:45:15 +0100

    user page

--- src/views/user.vue
+++ src/views/user.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="user-profile flex space center">
-    <div class="user-nav">
-      <!-- <img src="" alt="" /> -->
+    <!-- <div class="user-nav">
+      <img src="" alt="" /> 
       <h2>Your Websites</h2>
       <ul v-for="website in websites" :key="website._id">
         <li>{{ website.name }}</li>
@@ -9,10 +9,12 @@
       <router-link to="/templates">
         <div class="nav-create pointer">+ Create new website</div></router-link
       >
-    </div>
+    </div> -->
     <div class="main-section">
-      <h1>Welcome back, {{ user.username }}!</h1>
-      <h2 class="main-section-title">Your Websites:</h2>
+      <h3>Welcome back, {{ user.username }}!</h3>
+      <p class="main-section-title">
+        Select a site to edit, view and open its dashboard
+      </p>
       <div class="flex space center user-tmp-list">
         <span
           class="user-tmp-display"
